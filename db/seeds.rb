53185ef330f4e2bce82e52dc53aca763748642b0
hannahgreen1 <hannahgreen94@icloud.com>
2018-05-07 10:06:09 +0100

    seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,55 +3,27 @@ require_relative( "../models/country.rb" )
 require_relative( "../models/visit.rb" )
 require("pry-byebug")
 
-# Biting.delete_all()
-# Zombie.delete_all()
-# Victim.delete_all()
-#
-# zombie1 = Zombie.new({
-#   "name" => "Romero",
-#   "type" => "Generic"
-# })
-#
-# zombie1.save()
-#
-# zombie2 = Zombie.new({
-#   "name" => "Raimi",
-#   "type" => "Deadite"
-# })
-#
-# zombie2.save()
-#
-# zombie3 = Zombie.new({
-#   "name" => "Boyle",
-#   "type" => "Runner"
-# })
-#
-# zombie3.save()
-#
-# zombie4 = Zombie.new({
-#   "name" => "Newell",
-#   "type" => "Boomer"
-# })
-#
-# zombie4.save()
-#
-# victim1 = Victim.new({
-#   "name" => "Maggie",
-#   "run_speed" => 12
-# })
-#
-# victim1.save()
-#
-# victim2 = Victim.new({
-#   "name" => "Rick",
-#   "run_speed" => 11
-# })
-#
-# victim2.save()
-#
-# victim3 = Victim.new({
-#   "name" => "Michonne",
-#   "run_speed" => 15
-# })
-# 
-# victim3.save()
+city1 = City.new({
+  "name" => "Edinburgh"
+  })
+
+city2 = City.new({
+  "name" => "Rome"
+  })
+
+city3 = City.new({
+  "name" => "Malaga"
+  })
+
+
+country1 = Country.new({
+  "name" => "Scotland"
+  })
+
+country2 = Country.new({
+  "name" => "Italy"
+  })
+
+country3 = Country.new({
+  "name" => "Spain"
+  })
